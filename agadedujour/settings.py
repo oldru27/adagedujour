5683b36b5f68416161d821b165cda452a5eafd4c
oldru27 <o.druais@gmail.com>
2018-09-10 11:46:30 +0200

    nouvelle BD

--- agadedujour/settings.py
+++ agadedujour/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jobs.apps.JobsConfig', #dossier job>apps> class JobsConfig
+    'jobs.apps.JobsConfig', #dossier job>apps> class JobsConfig,
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'agadedujour.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adagedb',
+        'USER':'postgres',
+        'PASSWORD':'totodu27!',
+        'HOST':'localhost',
+        'PORT':'5433',
     }
 }
 
