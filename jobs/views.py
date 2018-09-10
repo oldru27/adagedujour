40b7fcab2d4d097d39e92d749e0af416ad20a079
oldru27 <o.druais@gmail.com>
2018-09-10 23:25:46 +0200

    page d'attente

--- jobs/views.py
+++ jobs/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello')
+    return HttpResponse("L'amour n'est pas dans le pré, il est dans le lointain - Site en construction")
