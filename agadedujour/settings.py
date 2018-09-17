d5be372c45033c1c112aa674b34cd63d4eef91b1
oldru27 <o.druais@gmail.com>
2018-09-18 00:01:30 +0200

    nouvelle page blog

--- agadedujour/settings.py
+++ agadedujour/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'agadedujour/static/')
+   os.path.join(BASE_DIR, 'agadedujour/static/'),
+   os.path.join(BASE_DIR, 'jobs/static/')
  ]
 
 
--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class job(models.Model):
+    titre = models.CharField(max_length=150, default='')
     imagefun= models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    summary = models.TextField(default='Une description courte')
+    fullsummary = models.TextField(default='Un récit')
+    date = models.DateTimeField(default=datetime.now, blank=True)
